ac6416135d95f9575841642bd3f197e45166a466
Nacho Caiafa <nachokai@outlook.com>
2020-03-29 19:41:41 -0300

    Updates

---
+++ precache-manifest.eda9a9abdad0803b506b41217689a95c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4398681b203ab50502e30856fccfd4",
+    "url": "/Ifytops-v2/index.html"
+  },
+  {
+    "revision": "d5ad766e54c10e5ee5db",
+    "url": "/Ifytops-v2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5221ae5471ede06b0915",
+    "url": "/Ifytops-v2/static/js/2.37457559.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ifytops-v2/static/js/2.37457559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ad766e54c10e5ee5db",
+    "url": "/Ifytops-v2/static/js/main.8581bb4e.chunk.js"
+  },
+  {
+    "revision": "597ab199100adf937a92",
+    "url": "/Ifytops-v2/static/js/runtime-main.df28f7b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ifytops-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
