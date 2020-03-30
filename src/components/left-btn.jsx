673f6fcd766a79c10bc74b5e1ad920b3aa9dd7a9
Nacho Caiafa <nachokai@outlook.com>
2020-03-30 17:17:31 -0300

    Update left-btn.jsx

--- src/components/left-btn.jsx
+++ src/components/left-btn.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-function LeftBtn(props) {
-	return (
-		<a className="nav-links" href={props.link} id="inicio-btn">
-			<i className={props.icon}></i>
-			<span>{props.title}</span>
-		</a>
-	);
-}
+const LeftBtn = props => (
+	<a className="nav-links" href={props.link} id="inicio-btn">
+		<i className={props.icon}></i>
+		<span>{props.title}</span>
+	</a>
+);
+
 export default LeftBtn;
